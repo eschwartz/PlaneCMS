c01517c29c4add7113df7e5e484a97d5d7dbadd3
Edan Schwartz <eschwartz@weathernation.net>
2015-02-23 14:08:20 -0600

    some notes

--- test/PlaneCmsTest/Functional/HtmlViews/PicoCompatibilityTest.php
+++ test/PlaneCmsTest/Functional/HtmlViews/PicoCompatibilityTest.php
@@ -6,4 +6,9 @@ namespace PlaneCmsTest\Functional\HtmlViews;
 
 class PicoCompatibilityTest {
 
+	/**
+	 * I'm thinking that Pico compatibility should be
+	 *  implemented in a plugin, so that we aren't
+	 *  locked into a legacy interface.
+	 */
 }
